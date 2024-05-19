d848bbb1bd088ca3fbdde0c99d28440bb438ceee
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2024-05-20 01:46:43 +0700

    integrating upload

--- art-work/modules/artWork/addArtWork.tsx
+++ art-work/modules/artWork/addArtWork.tsx
@@ -1,12 +1,21 @@
 import { NextPage } from "next";
 import { ReactElement, useState } from "react";
 import { FaPlus } from "react-icons/fa6";
-import { UploadOutlined } from "@ant-design/icons/lib";
+import { InboxOutlined } from "@ant-design/icons/lib";
 import type { UploadProps } from "antd/lib";
-import { message, Modal, Upload } from "antd/lib";
+import { Button, message, Modal, Upload } from "antd/lib";
 import { useRouter } from "next/router";
+import { creatArtworks } from "../profile/api";
 
 const AddArtWorkModule: NextPage = (): ReactElement => {
+  const { Dragger } = Upload;
+  const [user, setUser] = useState<any>();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [socialMedia, setSocialMedia] = useState("");
+  const [image, setImage] = useState();
+  const [uploading, setUploading] = useState<boolean>(false);
+
   const router = useRouter();
   const [isModalAddArtWorkOpen, setisModalAddArtWorkOpen] = useState(false);
 
@@ -15,21 +24,56 @@ const AddArtWorkModule: NextPage = (): ReactElement => {
   };
 
   const props: UploadProps = {
-    name: "file",
-    action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
-    headers: {
-      authorization: "authorization-text",
-    },
+    name: "files",
+    showUploadList: false,
     onChange(info) {
-      if (info.file.status !== "uploading") {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === "done") {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === "error") {
-        message.error(`${info.file.name} file upload failed.`);
-      }
+      setImage(info.file.originFileObj as any);
+      console.log(info.file.originFileObj);
     },
+  };
+  const handleUpload = async (event: any) => {
+    event.preventDefault();
+
+    setUploading(true);
+
+    const formData = new FormData();
+    formData.append("files", image as any);
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/upload`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.ok) {
+      const result = await response.json();
+      alert("Artwork uploaded successfully!");
+      setImage(result[0].id);
+      setUploading(false);
+    } else {
+      console.error("Error uploading artwork");
+    }
+  };
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const data = {
+      data: {
+        name: title,
+        image: [image],
+        description: description,
+        user: user.id, // Assume user ID is 1 for this example
+        social_media_url: socialMedia,
+      },
+    };
+
+    const response = creatArtworks(data);
+
+    if (response.ok) {
+      const result = await response.json();
+      alert("Artwork uploaded successfully!");
+      console.log("Success:", result);
+    } else {
+      console.error("Error uploading artwork");
+    }
   };
   return (
     <div className=" flex justify-end">
@@ -63,6 +107,10 @@ const AddArtWorkModule: NextPage = (): ReactElement => {
                 name="title"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
                 required
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
             <div className="mb-3 ">
@@ -76,6 +124,9 @@ const AddArtWorkModule: NextPage = (): ReactElement => {
                 name="desc"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
                 required
+                id="desc"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
             </div>
             <div className="mb-3 ">
@@ -89,28 +140,36 @@ const AddArtWorkModule: NextPage = (): ReactElement => {
                 name="sosmed"
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm"
                 required
+                type="text"
+                id="social-media"
+                value={socialMedia}
+                onChange={(e) => setSocialMedia(e.target.value)}
               />
             </div>
             <label className="block text-sm font-medium text-gray-700 mb-2">
               Upload Image
             </label>
-            <Upload {...props}>
-              <button className=" flex items-center gap-3 ">
-                <UploadOutlined />
-                Click to Upload
-              </button>
-            </Upload>
-            <div className=" flex gap-3 justify-end">
-              <button
-                onClick={() => router.back()}
-                className=" mt-6 font-[600] bg-transparent border-black border-2  text-black py-2 px-5 rounded-md shadow-sm hover:bg-black hover:text-white  focus:outline-none focus:ring-2 focus:ring-offset-2 "
-              >
-                Cancel
-              </button>
-              <button className=" mt-6 font-[600] bg-black text-white py-2 px-5 rounded-md shadow-sm hover:bg-black hover:text-black hover:border-2 hover:border-black focus:outline-none focus:ring-2 focus:ring-offset-2 ">
-                Submit
-              </button>
-            </div>
+            <Dragger {...props}>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">
+                Click or drag file to this area to upload
+              </p>
+              <p className="ant-upload-hint">
+                Support for a single or bulk upload. Strictly prohibited from
+                uploading company data or other banned files.
+              </p>
+            </Dragger>
+            <Button onClick={handleUpload} disabled={!image || uploading}>
+              {uploading ? "Uploading..." : "Start Upload"}
+            </Button>
+            <button
+              onClick={handleSubmit}
+              className="w-full mt-6 font-[600] bg-gray-800 text-white py-2 px-4 rounded-md shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 "
+            >
+              Submit
+            </button>
           </form>
         </div>
       </Modal>
