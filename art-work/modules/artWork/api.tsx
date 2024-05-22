524d32c6edce185b2eac68e7b5f8c8d24b641160
GarnesPratama <97274527+GarnesPratama@users.noreply.github.com>
2024-05-22 21:50:48 +0700

    fix get artworks

--- art-work/modules/artWork/api.tsx
+++ art-work/modules/artWork/api.tsx
@@ -2,28 +2,15 @@ import axios from "axios";
 
 export const getArtworks = async () => {
   const url = `${process.env.NEXT_PUBLIC_API_URL}/artworks?populate=*`;
-  const config = {
-    headers: {
-      Authorization: `Bearer ${JSON.parse(
-        localStorage.getItem("token") as string
-      )}`,
-    },
-  };
-  const data = await axios.get(url, config);
+  const data = await axios.get(url);
 
   return data.data;
 };
 
 export const getArtworksById = async (id: any) => {
   const url = `${process.env.NEXT_PUBLIC_API_URL}/artworks/${id}?populate=*`;
-  const config = {
-    headers: {
-      Authorization: `Bearer ${JSON.parse(
-        localStorage.getItem("token") as string
-      )}`,
-    },
-  };
-  const data = await axios.get(url, config);
+
+  const data = await axios.get(url);
 
   return data.data;
 };
